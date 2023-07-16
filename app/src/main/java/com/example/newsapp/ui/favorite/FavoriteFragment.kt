66334c3ed087ce133efc5169ae6055a34cc91cca
DenKli <https://github.com/DenK1987/AN10-onl>
2023-07-16 15:33:09 +0300

    Lesson-2

---
+++ app/src/main/java/com/example/newsapp/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.newsapp.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment() {
+
+    private lateinit var binding: FragmentFavoriteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
