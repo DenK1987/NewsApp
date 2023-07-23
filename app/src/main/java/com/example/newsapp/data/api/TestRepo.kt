72582bc164d03e9aaf1c9be198382ff476ee3a0c
DenKli <https://github.com/DenK1987/AN10-onl>
2023-07-23 17:47:57 +0300

    Lesson-5

--- app/src/main/java/com/example/newsapp/data/api/TestRepo.kt
+++ app/src/main/java/com/example/newsapp/data/api/TestRepo.kt
@@ -2,7 +2,7 @@ package com.example.newsapp.data.api
 
 import javax.inject.Inject
 
-class TestRepo @Inject constructor(private val newsService: NewsService) {
-
-    suspend fun getAll() = newsService.getHeadlines()
-}
+//class TestRepo @Inject constructor(private val newsService: NewsService) {
+//
+//    suspend fun getAll() = newsService.getHeadlines()
+//}
--- app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
@@ -1,13 +1,19 @@
 package com.example.newsapp.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.databinding.FragmentMainBinding
+import com.example.newsapp.ui.adapters.NewsAdapter
+import com.example.newsapp.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_main.*
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
@@ -16,6 +22,7 @@ class MainFragment : Fragment() {
 
     //    private val viewModel: MainViewModel by viewModels()
     private val viewModel by viewModels<MainViewModel>()
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +34,35 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.all
+//        viewModel.all
+
+        initAdapter()
+        viewModel.newsLiveData.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is Resource.Success -> {
+                    progress_bar.visibility = View.INVISIBLE
+                    response.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    progress_bar.visibility = View.INVISIBLE
+                    response.data?.let {
+                        Log.e("KEK", "MainFragment: error: $it")
+                    }
+                }
+                is Resource.Loading -> {
+                    progress_bar.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        news_adapter.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
